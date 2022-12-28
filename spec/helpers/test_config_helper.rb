673d56535abca55aa660e24bb9611b17c9158f0b
youthink0 <a0985418957@gmail.com>
2022-12-28 03:33:27 -0800

    Adjust STEAM_ID comment in test

--- spec/helpers/test_config_helper.rb
+++ spec/helpers/test_config_helper.rb
@@ -6,6 +6,6 @@ require 'yaml'
 
 CONFIG = YAML.safe_load(File.read('config/secrets.yml'))['test']
 STEAM_KEY = CONFIG['STEAM_KEY']
-STEAM_ID = CONFIG['STEAM_ID']
+STEAM_ID = '76561198326876707'
 USERNAME = 'Cherise'
 CORRECT = YAML.safe_load(File.read('spec/fixtures/steam_results.yml'))
